16f2a7a57717029db441a76c4bed49392ce39f94
randomxusr <95206740+randomxusr@users.noreply.github.com>
2023-01-05 22:11:19 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+Welcome.sh is a simple script that Welcomes a user
+
+And prints some useful system information for quick referene.
+
+You may Distribute and use this script according to the GPLv2
+
+This script is original to extent of the tools arranged and the options
+with which are used to output the information targetted by the script.
+
+BASH, and all tools used in this script are trademarked and owned by their
+respective maintainers.
+
+In order to run the Welcome.sh script, please perform the following at the command line:
+
+cd into the directory where the script was downloaded.
+
+and change permissions as follows:
+
+chmod +x Welcome.sh
+
+Run the file as normal -
+./Welcome.sh from the command line.
+
+You Are free to Modify and edit this script to suit your needs. You may redistribute 
+this script crediting the original Author: randomxusr
+
+All rights reserved.
+
---
+++ Welcome.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#This is a Welcome script designed to provide some using system information to users.
+#All rights reserved, randomxusr
+
+#Display a Welcome message to the user
+
+echo "**************************************************Welcome $(whoami)************************************************************************"
+
+#output the date
+echo "The date is: $(date +"%a %B %d")"
+
+#outpuut the Time
+echo "Local time is: $(date +"%r")"
+
+echo \
+
+#output the uptime
+echo "The system has been running for: " 
+uptime -p | cut --complement -b 1-3
+
+echo \
+
+#output the logged in user info
+w
+
+echo \
+
+#output the last few logins
+last -n 5
+
+echo  \
+
+#add some info about disk space.
+#Set some variables for Used and Total Disk Space
+
+Used="$(df -h --output=used /dev/sdg3 | grep G)"
+Total="$(df -h --output=size /dev/sdg3 | grep G)"
+
+#Memory Variables
+
+used=$(free -m | sed -n '2p' | awk '{print $3}')
+free=$(free -m | sed -n '2p' | awk '{print $4}')
+
+
+#Print a message about Ram and Disk space usage
+echo Disk and RAM usage: 
+echo \
+
+# Display the Used and Total Disk Space and Ram
+echo Used/Total Disk space:  $Used / $Total
+echo "Used/Free Memory: $((used/1024)) GB / $((free/1024)) GB"
+
+echo \
+
+
+#Output the 10 Highest usage processes from Top
+top -b -e g -n 1 | sed -n 1,17p
+
